049250b2b6d9b7984883a37f645bf9548445b991
akhan445 <abdullahshamskhan@gmail.com>
2021-05-10 01:58:37 -0400

    Added profile, updated ports

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ require('./database/database.js');
 
 app.use(routes);
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server running on port 3000");
 })
